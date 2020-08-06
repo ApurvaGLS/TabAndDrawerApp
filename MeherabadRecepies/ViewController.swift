944b24e176950e52bee38c0f4e38ada4cb83145c
Apurva Dongre <apurva@giantleapsystems.com>
2020-08-06 11:11:33 +0530

    Initial commit

---
+++ MeherabadRecepies/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  MeherabadRecepies
+//
+//  Created by Apurva Dongre on 06/08/20.
+//  Copyright © 2020 Meherabad Recepies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var barButtonItem: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        addLeftMenuButton()
+    }
+
+    func setupNavigationBar() {
+       self.navigationController?.navigationBar.isHidden = false
+       self.navigationController?.navigationBar.topItem?.title = "Home"
+       self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false;
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white];
+       self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationController?.navigationBar.barTintColor = UIColor.red
+       self.navigationController?.navigationBar.isTranslucent = false
+       self.extendedLayoutIncludesOpaqueBars = true
+       self.edgesForExtendedLayout = []
+       self.navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+     }
+    
+    func addLeftMenuButton() {
+        let menuBtn = UIButton(type: .custom)
+        menuBtn.setImage(UIImage(named: "Menu"), for: .normal)
+        menuBtn.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        menuBtn.addTarget(self, action: #selector(self.menuButtonPressed), for: .touchUpInside)
+        barButtonItem = UIBarButtonItem(customView: menuBtn)
+        self.navigationController?.navigationBar.topItem?.leftBarButtonItem = barButtonItem
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem?.tintColor = UIColor.clear
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem?.isEnabled = false
+    }
+    
+    @objc func menuButtonPressed() {
+        print("Button Pressed")
+    }
+}
+
