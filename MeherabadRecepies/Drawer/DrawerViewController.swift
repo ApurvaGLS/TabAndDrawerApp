ee5fb6d3a8f978ce7845d0943374000431675ac2
Apurva Dongre <apurva@giantleapsystems.com>
2020-08-06 16:58:55 +0530

    commit

--- MeherabadRecepies/Drawer/DrawerViewController.swift
+++ MeherabadRecepies/Drawer/DrawerViewController.swift
@@ -12,20 +12,14 @@ class DrawerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationController?.interactivePopGestureRecognizer!.isEnabled = false
+        self.navigationController?.navigationBar.isHidden = true
         print("In Drawer")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        
     }
-    */
 
 }
+
